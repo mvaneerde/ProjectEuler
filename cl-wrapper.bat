227188e6b14367f52972833d7f737bc38a48fe05
Matthew van Eerde (^_^) <mateer@microsoft.com>
2020-10-08 18:04:54 -0700

    Update cl-wrapper to work with new Visual Studio layout

--- cl-wrapper.bat
+++ cl-wrapper.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 
-set visual_cplusplus=%programfiles(x86)%\Microsoft Visual Studio 14.0\VC
-set kit_inc=%programfiles(x86)%\Windows Kits\10\Include\10.0.10586.0
-set kit_lib=%programfiles(x86)%\Windows Kits\10\Lib\10.0.10586.0
+set visual_cplusplus=%programfiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110
+set kit_inc=%programfiles(x86)%\Windows Kits\10\Include\10.0.18362.0
+set kit_lib=%programfiles(x86)%\Windows Kits\10\Lib\10.0.18362.0
 
 set INCLUDE=%visual_cplusplus%\include
 set INCLUDE=%INCLUDE%;%kit_inc%\um
 set INCLUDE=%INCLUDE%;%kit_inc%\ucrt
 set INCLUDE=%INCLUDE%;%~dp0Library
 
-set LIB=%visual_cplusplus%\lib
+set LIB=%visual_cplusplus%\lib\x86
 set LIB=%LIB%;%kit_lib%\um\x86
 set LIB=%LIB%;%kit_lib%\ucrt\x86
 
@@ -18,7 +18,12 @@ rem /TP means treat the files being compiled as C++
 rem rather than inferring the type from the extension
 rem
 rem /EHsc allows using things like std::vector
-call "%visual_cplusplus%\bin\cl.exe" ^
+if /i "%processor_architecture%"=="amd64" (
+	set cl_arch=x64
+) else (
+	set cl_arch=x86
+)
+call "%visual_cplusplus%\bin\Host%cl_arch%\%cl_arch%\cl.exe" ^
 	/EHsc ^
 	/nologo ^
 	/TP ^
